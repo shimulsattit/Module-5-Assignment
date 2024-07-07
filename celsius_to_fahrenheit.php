8b238e3421387de5e673e137e43130224a0f4292
Md Shimul Hossain <74421275+shimulsattit@users.noreply.github.com>
2024-07-07 20:53:47 +0600

    Add files via upload

---
+++ celsius_to_fahrenheit.php
@@ -0,0 +1,10 @@
+<?php
+// Temperature in Celsius
+$celsius = 32;
+
+// Convert Celsius to Fahrenheit
+$fahrenheit = ($celsius * 9/5) + 32;
+
+// Print the result with 2 decimal points
+printf("The temperature %.2f degrees Celsius is equivalent to %.2f degrees Fahrenheit.", $celsius, $fahrenheit);
+?>
